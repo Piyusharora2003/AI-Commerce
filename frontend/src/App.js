652cb511c6fe6f2df016875833b945462ac2214c
Piyush Arora <74443858+Piyusharora2003@users.noreply.github.com>
2023-06-27 08:34:51 +0530

    New nav introduced

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,6 +36,7 @@ import UpdateUser from './components/Admin/UpdateUser';
 import ReviewsTable from './components/Admin/ReviewsTable';
 import Wishlist from './components/Wishlist/Wishlist';
 import NotFound from './components/NotFound';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
@@ -67,6 +68,7 @@ function App() {
   
   return (
     <>
+      <Navbar/>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
